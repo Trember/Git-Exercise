dd8230a428a2e9f3b84491afb77fb0a61a22aad8
Trember <97796576+Trember@users.noreply.github.com>
2023-10-22 20:44:18 +0300

    Globaled Convertor

--- GIT_Ex.py
+++ GIT_Ex.py
@@ -1,23 +1,21 @@
 class AnalogConvertor:
-    def __init__(self, analog_volt):
+    def __init__(self, analog_volt,max_volt,bits):
         self.analog_volt = analog_volt
+        self.max_volt = max_volt
+        self.bits = bits
 
     def ToDigital(self):
-        power_percentage = self.analog_volt/5
+        power_percentage = self.analog_volt/self.max_volt
         if power_percentage == 1:
-            return bin(2**10-1)[2:]
-        return bin(int((2**10) * power_percentage))[2:].zfill(10)
+            return bin(2**self.bits-1)[2:]
+        return bin(int((2**self.bits) * power_percentage))[2:].zfill(self.bits)
     
     def set_digital_value(self, value):
         self.analog_volt = value
-    
-    def padded_bin(self,i, width):
-        s = bin(i)
-        return s[:2] + s[2:].zfill(width)
 
 if __name__ == '__main__':
-    convertor = AnalogConvertor(5)
+    convertor = AnalogConvertor(5,10,12)
     print(convertor.ToDigital())
-    convertor.set_digital_value(1)
+    convertor.set_digital_value(2.5)
     print(convertor.ToDigital())
 
